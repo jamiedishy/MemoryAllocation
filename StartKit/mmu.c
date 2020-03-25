13d9672769f24e53e28b5c60d47202a707104f38
jamiedishy <jamiedishy@gmail.com>
2020-03-24 23:56:24 -0400

    almost done

--- StartKit/mmu.c
+++ StartKit/mmu.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-testststs
+
 #define OFFSET_VALUE 0XFF
 #define PAGENUM_VALUE 0XFFFF
 
@@ -9,28 +9,30 @@ testststs
 // char page_number[8];
 int tlb_table[16][2];
 int page_table[256];
-char physical_memory[256][256];
+char physical_memory[128][256];
 int lru_counter[256]; // to keep track of least recently used
 
 int tlb_first = -1;
 int tlb_rear = -1;
 int mem_index = 0;
+int counter = 1;
 
-int page_fault_rate = 0;
-int tlb_hit_rate = 0;
-int total_counter = 0;
+int page_faults = 0;
+int tlb_hits = 0;
+// int total_counter = 0;
 
 char buffer[256];
 
 int main()
 {
-    char input_read[8];  // holds each line in a file
-    
+    char input_read[8]; // holds each line in a file
+
     int virtual_address; // its the integer version of the input_read
     int page_number;
     int offset_number;
     int physical_address;
     int value_in_physical_memory;
+    int frame_number;
 
     FILE *virtual_address_file_pointer;
     FILE *physical_address_file_pointer;
@@ -54,63 +56,131 @@ int main()
 
     while (fgets(input_read, sizeof(input_read), virtual_address_file_pointer) != 0)
     {
-        total_counter++;
+        // total_counter++;
 
         virtual_address = atoi(input_read);
         page_number = (virtual_address & PAGENUM_VALUE) >> 8;
         offset_number = virtual_address & OFFSET_VALUE;
 
-        int frame_number = check_tlb(page_number);
+        // fprintf(physical_address_file_pointer, "the page number is %d\n", page_number);
+        lru_counter[page_number] = counter;
+        counter++;
+
+        frame_number = check_tlb(page_number);
 
         if (frame_number != -1)
         { // page_number and frame_number are in TLB
-            tlb_hit_rate++;
-            physical_address = ((frame_number & 0X000000FF ) << 8) | (offset_number & 0X000000FF);
+            tlb_hits++;
+            physical_address = ((frame_number & 0X000000FF) << 8) | (offset_number & 0X000000FF);
             value_in_physical_memory = physical_memory[frame_number][offset_number];
         }
 
         else
-        {                                                                                                     
-            int frame_number = check_page_table(page_number); // check if page_number in page table
-            // printf("the frame num is %d\n", frame_number);
+        {
+            frame_number = check_page_table(page_number); // check if page_number in page table
+
             if (frame_number != -1)
             { // page number exists in page table
-                physical_address = ((frame_number & 0X000000FF ) << 8) | (offset_number & 0X000000FF);
-              
+                physical_address = ((frame_number & 0X000000FF) << 8) | (offset_number & 0X000000FF);
+
                 value_in_physical_memory = physical_memory[frame_number][offset_number];
 
                 // update TLB bc the page_number is not in TLB
-                // update_tlb(page_number, frame_number); // FIFO
+                update_tlb(page_number, frame_number); // FIFO
             }
             else
-            { // page_number NOT in page table... page fault
-                page_fault_rate++;
-                int page_address = page_number * 256; 
+            { // frame_number NOT in page table (-1) ... page fault
+                page_faults++;
 
-                fseek(backing_store_pointer, page_address, SEEK_SET);
-                fread(buffer, sizeof(char), 256, backing_store_pointer);
-
-                for (int i = 0; i < 256; i++)
+                if (mem_index < 128)
                 {
-                    physical_memory[mem_index][i] = buffer[i];
+                    int page_address = page_number * 256;
+
+                    fseek(backing_store_pointer, page_address, SEEK_SET);
+                    fread(buffer, sizeof(char), 256, backing_store_pointer);
+
+                    for (int i = 0; i < 256; i++)
+                    {
+                        physical_memory[mem_index][i] = buffer[i];
+                    }
+
+                    frame_number = mem_index;
+                    physical_address = ((frame_number & 0X000000FF) << 8) | (offset_number & 0X000000FF);
+                    value_in_physical_memory = physical_memory[frame_number][offset_number];
+
+                    page_table[page_number] = frame_number;
+
+                    update_tlb(page_number, frame_number);
+
+                    mem_index++;
                 }
-                frame_number = mem_index; 
-                physical_address = ((frame_number & 0X000000FF ) << 8) | (offset_number & 0X000000FF);
-                value_in_physical_memory = physical_memory[frame_number][offset_number];
-                page_table[page_number] = frame_number;
-                //update_tlb(page_number, frame_number);
-                mem_index++;
+
+                else // no more free frames
+                {    // if mem_index is greater than physical_memory size and hence no more available frames
+                    // what's the least recently used page_number? set page_table[LRU_page_number] = -1 and page_table[page_number] = that frame
+
+                    int temp_page_number = get_LRU();
+
+                    page_table[page_number] = page_table[temp_page_number]; // printf("value in temppagenum fram is %d\n", page_table[temp_page_number]);
+
+                    frame_number = page_table[page_number];
+                    // fprintf(physical_address_file_pointer, "were in;)))\n");
+
+                    int page_address = page_number * 256;
+
+                    fseek(backing_store_pointer, page_address, SEEK_SET);
+                    fread(buffer, sizeof(char), 256, backing_store_pointer);
+
+                    for (int i = 0; i < 256; i++)
+                    {
+                        physical_memory[frame_number][i] = buffer[i];
+                    }
+
+                    // fprintf(physical_address_file_pointer, "the frame num and offset num are %d %d\n", frame_number, offset_number);
+                    physical_address = ((frame_number & 0X000000FF) << 8) | (offset_number & 0X000000FF);
+                    // fprintf(physical_address_file_pointer, "physical address and the other half is %d %d\n", physical_address,  ((frame_number & 0X000000FF) << 8) | (offset_number & 0X000000FF));
+
+                    value_in_physical_memory = physical_memory[frame_number][offset_number];
+
+                    page_table[temp_page_number] = -1;
+
+                    lru_counter[temp_page_number] = 1000000;
+
+                    update_tlb(page_number, frame_number);
+                }
+                // fprintf(physical_address_file_pointer, "the page number and frame numebr and offset number are %d %d %d\n",page_number, frame_number, offset_number);
             }
         }
 
-        fprintf(physical_address_file_pointer, "Virtual address: %d", virtual_address);
-        fprintf(physical_address_file_pointer, " Phyiscal address: %d", physical_address);
-        fprintf(physical_address_file_pointer, " Value: %d\n", value_in_physical_memory);
+        fprintf(physical_address_file_pointer, "logical: %d,", virtual_address);
+        fprintf(physical_address_file_pointer, " physical: %d,", physical_address);
+        fprintf(physical_address_file_pointer, " byte value: %d\n", value_in_physical_memory);
+
+        // fprintf(physical_address_file_pointer, "Virtual address: %d", virtual_address);
+        // fprintf(physical_address_file_pointer, " Phyiscal address: %d", physical_address);
+        // fprintf(physical_address_file_pointer, " Value: %d\n", value_in_physical_memory);
     }
-    printf("Page Fault Rate:        %d\n", page_fault_rate);
-    printf("TLB Hit Rate:           %d\n", tlb_hit_rate);
+    printf("Page Faults = %d\n", page_faults);
+    printf("TLB Hits = %d\n", tlb_hits);
 
     return 0;
+}
+
+int get_LRU()
+{
+    int minimum = 90000000;
+    int temp_page_num;
+    for (int i = 0; i < 256; i++)
+    {
+        //printf("the value in lrucounter si %d\n", lru_counter[i]);
+        if (lru_counter[i] < minimum)
+        {
+            minimum = lru_counter[i];
+            temp_page_num = i;
+        }
+    }
+    // printf( "the temp page num is %d and minimum is %d", temp_page_num, minimum);
+    return (temp_page_num);
 }
 
 void update_tlb(int pageNumber, int frameNumber)
@@ -126,13 +196,14 @@ void update_tlb(int pageNumber, int frameNumber)
     else
     {
         tlb_rear++;
-        if (tlb_rear > 15) { // from 0 to 15 ... size 16
+        if (tlb_rear > 15)
+        { // from 0 to 15 ... size 16
             tlb_rear = 0;
         }
-         tlb_table[tlb_rear][0] = pageNumber;
-         tlb_table[tlb_rear][1] = frameNumber;
+        tlb_table[tlb_rear][0] = pageNumber;
+        tlb_table[tlb_rear][1] = frameNumber;
     }
-   // return;
+    // return;
 }
 
 int check_page_table(int pageNumber)
@@ -163,8 +234,8 @@ void init()
     {
         page_table[i] = -1;
     }
-    // for (int i = 0; i < 256; i++)
-    // {
-    //     physical_memory[i] = 0;
-    // }
+    for (int i = 0; i < 256; i++)
+    {
+        lru_counter[i] = 1000000;
+    }
 }
